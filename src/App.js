de664ea8513d1ab7a36208d81023a93ff1b07ab2
Fathia Mohammed <mohammedfo5429@gmail.com>
2019-04-06 14:18:26 -0400

    created a nav bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link } from "react-router-dom"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div class="custom-padding">
+
+        <nav>
+          <div class="logo">Cristal Clear</div>
+
+              <ul class="menu-area">
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Services</a></li>
+                  <li><a href="#">Contact</a></li>
+              </ul>   
+        </nav>
+</div>
+
+
+
+
       </div>
     );
   }
